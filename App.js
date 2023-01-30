418f0352b9342fa490ccdc036b9602f869ac1a8e
Harpreet Saini <121703700+HarpreetSaini02@users.noreply.github.com>
2023-01-30 18:31:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const AppLayout = () => {
+
+    return(
+        <h1> This is my Project </h1>
+    )
+
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
